0c35494cd094fc30439fb1207afd38159c2c0597
Tomás Godoi <tomastxg@gmail.com>
2014-11-11 23:51:04 -0200

    Criando o GlyphTest.
    Criando o método de teste testGlyph que testa a instanciação de um
    Glyph, mas que ainda está falhando.

---
+++ src/test/java/br/com/txg/gof/lexi/core/GlyphTest.java
@@ -0,0 +1,14 @@
+package br.com.txg.gof.lexi.core;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GlyphTest {
+
+	@Test
+	public void testGlyph() {
+		Glyph glyph = new Glyph();
+		Assert.assertNotNull(glyph);
+	}
+	
+}
