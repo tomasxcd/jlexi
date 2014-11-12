f4969dd8b2e1f31415fd210b8eb07d03a8aa8cbd
Tomás Godoi <tomastxg@gmail.com>
2014-11-12 00:11:53 -0200

    Teste volta a passar.
    O método agora existe.

--- src/main/java/br/com/txg/gof/lexi/core/Glyph.java
+++ src/main/java/br/com/txg/gof/lexi/core/Glyph.java
@@ -2,4 +2,6 @@ package br.com.txg.gof.lexi.core;
 
 public interface Glyph {
 
+	void draw(Window window);
+
 }
--- src/main/java/br/com/txg/gof/lexi/core/LCharacter.java
+++ src/main/java/br/com/txg/gof/lexi/core/LCharacter.java
@@ -2,4 +2,8 @@ package br.com.txg.gof.lexi.core;
 
 public class LCharacter implements Glyph {
 
+	public void draw(Window window) {
+
+	}
+
 }
